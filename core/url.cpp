f84a83efc3ce019d62b2993358979df4089c5ef9
a012 <Aeagan0507@gmail.com>
2026-04-10 18:25:00 -0400

    Update url.cpp

--- core/url.cpp
+++ core/url.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <string>
+#include <curl/curl.h>
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
+    output->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 int main() {
     std::string url;
-
     std::cout << "Enter a URL: ";
-    std::getline(std::cin, url);   // user can type any URL
+    std::getline(std::cin, url);
 
-    // Remove protocol (http:// or https://)
-    std::string protocol = "https://";
-    size_t start = url.find(protocol);
-    if (start != std::string::npos) {
-        start += protocol.length();
-    } else {
-        protocol = "http://";
-        start = url.find(protocol);
-        if (start != std::string::npos)
-            start += protocol.length();
-        else
-            start = 0; // no protocol found
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        std::cerr << "Failed to initialize curl\n";
+        return 1;
     }
 
-    // Find the end of the domain
-    size_t end = url.find('/', start);
+    std::string response;
 
-    // Extract the forum/domain name
-    std::string forumName = url.substr(start, end - start);
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-    std::cout << "Forum name: " << forumName << "\n";
+    CURLcode res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    if (res != CURLE_OK) {
+        std::cerr << "Request failed\n";
+        return 1;
+    }
+
+    std::cout << "Page content:\n" << response << "\n";
 }
