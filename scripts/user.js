b94ab424917aa95c9d1e9e710d7f90f8fc9422d5
a012 <Aeagan0507@gmail.com>
2026-04-22 16:41:02 -0400

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -26,3 +26,4 @@ function randomRange(min, max) {
                 return min + Math.floor(Math.random() * (max - min + 1));
             else
                 return min + Math.random() * (max - min);
+}
